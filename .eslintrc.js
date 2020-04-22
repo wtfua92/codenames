24814bda8cd2ca7970bf718c7b3b8c6869313096
Andrii Tynok <atynok@homeaway.com>
2020-04-22 18:01:01 +0300

    Initial webpack setup

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/react",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+    browser: true,
+  },
+};
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.scss";
+
+ReactDOM.render(<React.StrictMode>test2</React.StrictMode>, document.getElementById("root"));
